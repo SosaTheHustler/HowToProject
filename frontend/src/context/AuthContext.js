277f1adec4d7f0ad0118f8e98135ae185a8345fb
Krishna <91717723+krishnakishor13@users.noreply.github.com>
2022-02-09 12:52:25 +0530

    added the backend api

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -25,7 +25,7 @@ export const AuthProvider = ({ children }) => {
   let loginUser = async (e) => {
     e.preventDefault();
 
-    let response = await fetch("http://127.0.0.1:8000/api/token/", {
+    let response = await fetch("https://howtobackendapi.herokuapp.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
